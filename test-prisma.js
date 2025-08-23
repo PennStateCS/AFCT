3af301d0d1f482ea3f93da35fd562c9a32cdf1f0
Jeff Chiampi <jdc308@psu.edu>
2025-08-23 18:44:46 -0400

    Clean up

--- test-prisma.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-console.log('Available models on prisma client:');
-console.log(Object.keys(prisma));
-
-// Close the connection
-prisma.$disconnect();
