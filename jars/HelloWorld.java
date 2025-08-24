43ec64fed91836d2495765a3ddd90b7a375e6913
Jeff Chiampi <jdc308@psu.edu>
2025-08-23 21:26:41 -0400

    Updated docker config

--- jars/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java!");
-        System.out.println("Arguments received: " + String.join(", ", args));
-        if (args.length > 0) {
-            System.out.println("First argument: " + args[0]);
-        }
-    }
-}
