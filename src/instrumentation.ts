b40702598848fdac5f002242344918da11d59dbc
Edwin Kimsal <kimsal.edwin@gmail.com>
2026-06-06 11:32:08 -0400

    Submission Queue: 5

---
+++ src/instrumentation.ts
@@ -0,0 +1,6 @@
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    const { startSubmissionWorker } = await import("./lib/submission-worker");
+    startSubmissionWorker();
+  }
+}
