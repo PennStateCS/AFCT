b1446857b83fdcbbdec2ec9176e71ae8ad67c608
Edwin Kimsal <kimsal.edwin@gmail.com>
2026-06-22 08:48:46 -0400

    Removed unnecessary import and field in db

---
+++ prisma/migrations/20260622124651_submission_date_removal/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `gradedAt` on the `Submission` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Submission" DROP COLUMN "gradedAt";
